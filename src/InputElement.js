0f11aa776ebf51a5c11ee96165d7ce7f5b1913e2
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2021-07-23 01:09:47 +0200

    Add basics for handling with input text and using buttons

--- src/InputElement.js
+++ src/InputElement.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './InputElement.css';
 
 class InputElement extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
+    handleInput(e) { // e steht fuere Event
+        console.log(e.target.value);
+    }
+
     render() {
         return (
             <div className="input">
                 <input className="search-input" placeholder="Name der Station"></input>
-                <button className="button-input">Suchen</button>
+                <button className="button-input" onClick={this.handleInput}>Suchen</button>
             </div>
         );
     }
